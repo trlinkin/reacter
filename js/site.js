b32bcd32f05eca734053179c03e7176ef62d9306
Gary Hetzel <garyhetzel@gmail.com>
2012-09-10 12:40:13 -0400

    Cleaning up nav

--- js/site.js
+++ js/site.js
@@ -7,8 +7,9 @@ $(document).ready(function(){
 function updateCurrentNavigationItem(uri){
   $('.navbar .nav li').each(function(i, item){
     console.log(item.firstElementChild['href'].split('/'), uri.pathname.split('/'));
-    nav_section = item.firstElementChild['href'].split('/')[2];
-    uri_section = uri.pathname.split('/')[4];
+
+    nav_section = item.firstElementChild['href'].split('/')[4];
+    uri_section = uri.pathname.split('/')[2];
 
     if(nav_section == uri_section)
     {
